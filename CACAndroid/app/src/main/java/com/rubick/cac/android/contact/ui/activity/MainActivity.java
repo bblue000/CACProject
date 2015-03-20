a4afe2aa0d0ac320c883a4f8a38241a71f8b83a5
YinYong <yy15151877621@126.com>
2015-03-20 18:54:24 +0800

    [Update] test animation

--- CACAndroid/app/src/main/java/com/rubick/cac/android/contact/ui/activity/MainActivity.java
+++ CACAndroid/app/src/main/java/com/rubick/cac/android/contact/ui/activity/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
+import com.nineoldandroids.view.animation.AnimatorProxy;
 import com.rubick.cac.android.R;
 import com.rubick.cac.android.contact.CACContactManager;
 import com.rubick.cac.android.contact.model.CACContactModel;
+import com.rubick.cac.android.contact.ui.widget.CACCardContainer;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -17,13 +22,47 @@ public class MainActivity extends Activity {
 
     @InjectView(R.id.btn)
     Button btn;
+    @InjectView(R.id.container)
+    CACCardContainer container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
         ButterKnife.inject(this);
+        ViewPropertyAnimator
+                .animate(container)
+                .scaleXBy(.25f)
+                .scaleYBy(.25f)
+                .rotationXBy(-75)
+                .setStartDelay(1000L)
+                .setDuration(500)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
 
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        ViewPropertyAnimator
+                                .animate(container)
+                                .rotationXBy(75)
+                                .setDuration(1000)
+                                .start();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                })
+                .start();
     }
 
     @OnClick(R.id.btn)
---
+++ CACAndroid/app/src/main/java/com/rubick/cac/android/contact/ui/widget/CACCardContainer.java
@@ -0,0 +1,30 @@
+package com.rubick.cac.android.contact.ui.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * <p/>
+ *
+ * Created by Yin Yong on 2015/3/20.
+ */
+public class CACCardContainer extends RelativeLayout {
+    public CACCardContainer(Context context) {
+        super(context);
+    }
+
+    public CACCardContainer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CACCardContainer(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+    }
+}
